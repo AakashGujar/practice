a18437971426bdefad6ece3e8f52dafb05902127
Bexst24 <aakashgujar2001@gmail.com>
2024-02-14 13:58:47 +0530

    first commit

---
+++ slime-type-structure/src/App.jsx
@@ -0,0 +1,32 @@
+import './index.css';
+
+function App() {
+  return (
+    <>
+      <main>
+        <div className="slime1"></div>
+        <div className="slime2"></div>
+        <div className="slime3"></div>
+        <div id="gooey"></div>
+      <div id="center">
+        <div id="left">
+          <h3>
+          You want my treasure? You can have it! I left everything I gathered together in one place. Now you just have to find it! <br />
+          - Gol D. Roger
+          </h3>
+        </div>
+        <div id="right">
+          <h1>
+          The <br /> 
+          One Piece <br /> 
+          does <br />
+          exist!
+          </h1>
+        </div>
+      </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
